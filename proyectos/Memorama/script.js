56d538001946acf9a00c5e4fc82e77b07deb4296
Brandon Ariel SM <brandonarielsalvador@gmail.com>
2023-11-04 00:48:24 -0600

    first commit

---
+++ proyectos/Memorama/script.js
@@ -0,0 +1,138 @@
+// Hacer que el usuario pierda con muchos movimientos
+// Inicialización de variables
+let tarjetasVolteadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let tiempoInicial = 60;
+let tiempo = 60;
+let tiempoRegresivoId = null;
+
+// Sonidos
+let clickAudio = new Audio("./sounds/click.wav");
+let loseAudio = new Audio("./sounds/lose.wav");
+let rightAudio = new Audio("./sounds/right.wav");
+let winAudio = new Audio("./sounds/win.wav");
+let wrongAudio = new Audio("./sounds/wrong.wav");
+
+// DOM
+let movimientosEl = document.getElementById("movimientos");
+let aciertosEl = document.getElementById("aciertos");
+let tiempoEl = document.getElementById("tiempoRestante");
+let replayEl = document.getElementById("jugarDeNuevo");
+let infoEl = document.querySelector("#comoJugar");
+
+// Generación de números aleatorios
+let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+numbers.sort(() => {
+    return Math.random() - 0.5;
+});
+console.log(numbers);
+
+// Eventos
+infoEl.addEventListener("click", comoJugar);
+
+// Funciones
+function contarTiempo() {
+    tiempoRegresivoId = setInterval(() => {
+        tiempo--;
+        tiempoEl.innerHTML = tiempo + " segundos";
+        if (tiempo == 0) {
+            clearInterval(tiempoRegresivoId);
+            gameOver(numbers);
+            loseAudio.play();
+        }
+    }, 1000);
+}
+function gameOver(numbers) {
+    for (let i = 0; i <= 15; i++) {
+        let tarjetaBloqueada = document.getElementById(i);
+        tarjetaBloqueada.innerHTML = `<img src="imgs/${numbers[i]}.png">`;
+        tarjetaBloqueada.classList.add("disabled");
+    }
+    Swal.fire({
+        icon: "error",
+        title: "Perdiste...",
+        text: "Inténtalo de nuevo!",
+    });
+    replayEl.classList.remove("hidden");
+}
+function comoJugar() {
+    Swal.fire({
+        icon: "info",
+        title: "¿Cómo jugar?",
+        text: "Habrán una serie de parejas de cartas boca abajo. Deberás voltear sucesivamente dos cartas, memorizando la ubicación de las mismas. Cuando se encuentren dos cartas idénticas que formen pareja, se sumará un punto. La partida terminará cuando estén todas las parejas encontradas.",
+    });
+}
+
+// Función Principal
+function voltear(id) {
+    if (temporizador == false) {
+        contarTiempo();
+        temporizador = true;
+    }
+
+    tarjetasVolteadas++;
+    console.log(tarjetasVolteadas);
+    if (tarjetasVolteadas == 1) {
+        // Mostrar primer número
+        tarjeta1 = document.getElementById(id);
+        primerResultado = numbers[id];
+        tarjeta1.innerHTML = `<img src="imgs/${primerResultado}.png">`;
+        clickAudio.play();
+
+        // Deshabilitar primer botón
+        tarjeta1.classList.add("disabled");
+    } else if (tarjetasVolteadas == 2) {
+        // Mostrar segundo número
+        tarjeta2 = document.getElementById(id);
+        segundoResultado = numbers[id];
+        tarjeta2.innerHTML = `<img src="imgs/${segundoResultado}.png">`;
+
+        // Deshabilitar segundo botón
+        tarjeta2.classList.add("disabled");
+
+        // Incrementar movimientos
+        movimientos++;
+        movimientosEl.innerHTML = movimientos;
+        if (primerResultado === segundoResultado) {
+            // Encerar contador tarjetas destapadas
+            tarjetasVolteadas = 0;
+            rightAudio.play();
+
+            // Aumentar los aciertos
+            aciertos++;
+            aciertosEl.innerHTML = aciertos;
+        } else {
+            // Mostrar momentáneamente
+            wrongAudio.play();
+            setTimeout(() => {
+                tarjeta1.innerHTML = " ";
+                tarjeta2.innerHTML = " ";
+                tarjeta1.classList.remove("disabled");
+                tarjeta2.classList.remove("disabled");
+                tarjetasVolteadas = 0;
+            }, 800);
+        }
+        if (aciertos == 8) {
+            clearInterval(tiempoRegresivoId);
+            aciertosEl.innerHTML = `${aciertos} 😱`;
+            tiempoEl.innerHTML = `Fantástico, solo te demoraste ${
+                tiempoInicial - tiempo
+            } segundos 🎉`;
+            movimientosEl.innerHTML = `${movimientos} 😎`;
+            Swal.fire({
+                icon: "success",
+                title: "¡Enhorabuena!",
+                text: "Haz ganado 🥳",
+            });
+            winAudio.play();
+            replayEl.classList.remove("hidden");
+        }
+    }
+}
---
+++ proyectos/piedra-papel-tijera/app.js
@@ -0,0 +1,90 @@
+const PIEDRA = "piedra";
+const PAPEL = "papel";
+const TIJERAS = "tijeras";
+const EMPATES = 0;
+const GANADOS = 1;
+const PERDIDOS = 2;
+
+const piedraBtn = document.getElementById("piedra");
+const papelBtn = document.getElementById("papel");
+const tijerasBtn = document.getElementById("tijeras");
+const resultText = document.getElementById("resultText");
+const machineImgEl = document.getElementById("machineImg");
+const userImgEl = document.getElementById("userImg");
+
+piedraBtn.addEventListener("click", () => {
+    play(PIEDRA);
+});
+papelBtn.addEventListener("click", () => {
+    play(PAPEL);
+});
+tijerasBtn.addEventListener("click", () => {
+    play(TIJERAS);
+});
+
+// Mostrar la opción elegida por el User
+// Y hacer que la CPU seleccione uno (con animación)
+function play(userOption) {
+    userImgEl.src = "imgs/" + userOption + ".png";
+    resultText.innerHTML = "Eligiendo...";
+
+    const interval = setInterval(function () {
+        piedraBtn.disabled = true;
+        papelBtn.disabled = true;
+        tijerasBtn.disabled = true;
+
+        const machineOption = cambiarTipo();
+        machineImgEl.src = "imgs/" + machineOption + ".png";
+    }, 120);
+
+    setTimeout(function () {
+        clearInterval(interval);
+        const machineOption = cambiarTipo();
+        const result = calcResult(userOption, machineOption);
+        machineImgEl.src = "imgs/" + machineOption + ".png";
+        switch (result) {
+            case EMPATES:
+                resultText.innerHTML = "Haz empatado :/";
+                break;
+            case GANADOS:
+                resultText.innerHTML = "¡Has Ganado, enhorabuena! :D";
+                break;
+            case PERDIDOS:
+                resultText.innerHTML = "Has perdido, inténtalo de nuevo ;(";
+                break;
+        }
+        piedraBtn.disabled = false;
+        papelBtn.disabled = false;
+        tijerasBtn.disabled = false;
+    }, 1200);
+}
+
+
+function cambiarTipo() {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            return PIEDRA;
+        case 1:
+            return PAPEL;
+        case 2:
+            return TIJERAS;
+    }
+}
+
+// Validación para saber quién ganó el partido
+function calcResult(userOption, machineOption) {
+    if (userOption === machineOption) {
+        return EMPATES;
+    } else if (userOption === PIEDRA) {
+        if (machineOption === PAPEL) return PERDIDOS;
+        if (machineOption === TIJERAS) return GANADOS;
+    } else if (userOption === PAPEL) {  // Agrega esta verificación para PAPEL
+        if (machineOption === PIEDRA) return GANADOS;
+        if (machineOption === TIJERAS) return PERDIDOS;
+    } else if (userOption === TIJERAS) {  // Agrega esta verificación para TIJERAS
+        if (machineOption === PIEDRA) return PERDIDOS;
+        if (machineOption === PAPEL) return GANADOS;
+    }
+}
+
